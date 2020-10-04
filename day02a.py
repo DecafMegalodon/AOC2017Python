6de87274c811e95de45d8aa62ad471924fdb17ce
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-04 11:53:46 -0400

    Day 02a complete

--- day02a.py
+++ day02a.py
@@ -1,7 +1,12 @@
 import fileinput
 
+rollingChecksum = 0
+
 for line in fileinput.input():
+    if(line == "\n"):
+        break
     splitNumbers = line.split()
     splitNumbers = [int(''.join(chars)) for chars in splitNumbers]
-    for datum in splitNumbers:
-        print(datum)
+    rollingChecksum += max(splitNumbers) - min(splitNumbers)
+
+print(rollingChecksum)
