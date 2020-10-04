3e398d83a63bd84bf34696a234bcd40ba646cfa4
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-04 12:12:46 -0400

    Basic comparisons

--- day02b.py
+++ day02b.py
@@ -5,8 +5,10 @@ rollingChecksum = 0
 for line in fileinput.input():
     if(line == "\n"):
         break
-    splitNumbers = line.split()
-    splitNumbers = [int(''.join(chars)) for chars in splitNumbers]
-    rollingChecksum += max(splitNumbers) - min(splitNumbers)
+    nums = line.split()
+    nums = [int(''.join(chars)) for chars in nums]
+    for first in range(0, len(nums)):
+        for second in range(first+1, len(nums)):
+            print("%d ? %d" % (nums[first], nums[second]))
 
 print(rollingChecksum)
