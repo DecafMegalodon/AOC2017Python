d767e1f5e544a15c36e562aa385d0e289b21d2a5
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-04 17:19:31 -0400

    Fixing some observations, plus some new ones

--- day03a.py
+++ day03a.py
@@ -2,10 +2,21 @@
 Addresses are 1-indexed
 A new "ring" starts on n^2 + 1 indices
 New "Rings" start near - but not on a corner. 1 away.
-The diameter of the current ring is ceiling(sqrt(index))
+We can find the ring number as so: ((math.sqrt(index-1)+1)//2)+1
+Ring diameter can be found by ring number*2-1
+We can break the current ring into four "quadrants" like so
+22221
+3$$$1
+3$$$1
+3$$$1
+34444
+
 """
 
 import fileinput
+import math
 
-input = int(fileinput.input()[0])
-print(input)
+index = int(fileinput.input()[0])
+
+ring = int(((math.sqrt(index-1)+1)//2)+1)
+print(ring)
