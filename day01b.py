852fb17f7526fa78663aae8405e40cd449c55e30
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-04 11:18:37 -0400

    Day 01b complete. Watch out for py3's division being float by default vs 2 being integer

---
+++ day01b.py
@@ -0,0 +1,15 @@
+import fileinput
+
+def getDigit(string, pos):
+    return int(string[pos % len(string)])
+
+input = fileinput.input()[0][:-1:] #input from AOC will always be on a single line, including a \n
+
+runningSum = 0
+
+for i in range(0, len(input)):
+    if(getDigit(input,i) == getDigit(input,i+len(input)//2)):
+        runningSum += getDigit(input,i)
+        
+print(runningSum)
+
