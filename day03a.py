b782bf69914147c42bd1e66e03c239824ad39f45
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-04 16:38:12 -0400

    Observations and planning for day 03a

--- day03a.py
+++ day03a.py
@@ -1,3 +1,10 @@
+"""Observations:
+Addresses are 1-indexed
+A new "ring" starts on n^2 + 1 indices
+New "Rings" start near - but not on a corner. 1 away.
+The diameter of the current ring is ceiling(sqrt(index))
+"""
+
 import fileinput
 
 input = int(fileinput.input()[0])
