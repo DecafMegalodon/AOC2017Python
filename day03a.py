ab21d6f2f4ca83d53483950c8312fa8dbf948497
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-05 12:07:31 -0400

    More observations. Should be enough to quickly derive most answers

--- day03a.py
+++ day03a.py
@@ -10,7 +10,10 @@ We can break the current ring into four "quadrants" like so
 3$$$1
 3$$$1
 34444
-
+Distance from the origin depends only on where we are in a particular quadrant for a given ring - not which one we're in.
+    Being in index 1 of quadrant 1 is equally far from the origin as index 1 of quadrant 2, 3, and 4
+We can find the end of a ring by knowing what ring we're in. (Ring*2-1) ** 2
+We can find the start or a ring by adding one to the end of the previous ring (save for ring one)
 """
 
 import fileinput
