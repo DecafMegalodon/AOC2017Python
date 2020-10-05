57ca464514e524587a7fccea63148c146685021d
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-05 16:44:55 -0400

    Day 03a complete. Some cleanup would be nice though.

--- day03a.py
+++ day03a.py
@@ -1,4 +1,5 @@
 """Observations:
+Some of these observations do not hold for the trivial case of index = 0.
 Addresses are 1-indexed
 A new "ring" starts on n^2 + 1 indices
 New "Rings" start near - but not on a corner. 1 away.
@@ -14,6 +15,11 @@ Distance from the origin depends only on where we are in a particular quadrant f
     Being in index 1 of quadrant 1 is equally far from the origin as index 1 of quadrant 2, 3, and 4
 We can find the end of a ring by knowing what ring we're in. (Ring*2-1) ** 2
 We can find the start or a ring by adding one to the end of the previous ring (save for ring one, which does not have a previous ring!)
+It's possible to calculate the number of elements in a ring by (Ring-1)*8
+The quadrant number can be derived from the ring start-ring end and dividing by 4.
+Quadrant size = ring circumferance / 4.
+The "center" of the quadrant - the one closest to the origin is the quadrantSize//2
+Distance from the origin = the ring number(-1) + distance from the "center" of the quadrant
 """
 
 import fileinput
@@ -23,4 +29,13 @@ index = int(fileinput.input()[0])
 
 ring = int(((math.sqrt(index-1)+1)//2)+1)
 print("Ring: ", ring)
-
+ringStart = ((ring-1)*2-1) ** 2 + 1
+print("Ring Start:", ringStart)
+ringEnd = ((ring)*2-1) ** 2
+print("Ring End:", ringEnd)
+ringDiameter = ring*2 - 1
+ringCircumf = (ring-1)*8
+print("Ring circumferance:", ringCircumf)
+quadrantPos = (index-ringStart)%(ringCircumf//4)
+print("Position within quadrant: ", quadrantPos)
+print("Distance from origin:", ring - 1 + abs((ringCircumf//8)-(quadrantPos+1)))
