840668746d780ae7e4f1313c60ed981b31ec1a51
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-06 09:42:50 -0400

    Day 04a started and finished

---
+++ day04a.py
@@ -0,0 +1,13 @@
+#Advent of code day 04a
+import fileinput
+
+validPasswords = 0
+
+for line in fileinput.input():
+    if(line == "\n"):
+        break
+    splitWords = line.split()
+    if len(splitWords) == len(set(splitWords)):
+        validPasswords += 1
+        
+print(validPasswords)
