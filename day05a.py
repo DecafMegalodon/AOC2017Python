20ff704572b94486e23eeecc1030dcb9b31b5004
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-06 11:03:33 -0400

    Day 05a complete!

--- day05a.py
+++ day05a.py
@@ -4,4 +4,13 @@ memory = [int(instruction)
             for instruction in fileinput.input() 
             if instruction != "\n"]
 
-print (memory)
+pc = 0 #Program counter
+steps = 0
+
+while pc < len(memory):
+    curMemory = memory[pc]
+    memory[pc] += 1
+    pc += curMemory
+    steps += 1
+    
+print(steps)
