cdfae6eaa5ff78a5ca934f1bbbf002f632ef2a61
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-06 09:36:21 -0400

    Some day 03b planning

---
+++ day03b.py
@@ -0,0 +1,9 @@
+"""
+Dev plan:
+Study the problem:
+    Our spiral described in OEIS as sequence A141481
+    Expands in all four cardinal directions
+Write spiral number generator
+Search for the answer
+"""
+import fileinput
