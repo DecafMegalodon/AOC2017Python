050f70620171d089c3f93dc8eacd2014f72bba86
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-06 09:52:59 -0400

    Day 04b completed!

--- day04b.py
+++ day04b.py
@@ -1,13 +1,20 @@
 #Advent of code day 04b
 import fileinput
 
+def split(string):
+    return [char for char in string]
+
 validPasswords = 0
 
 for line in fileinput.input():
     if(line == "\n"):
         break
-    splitWords = line.split()
-    if len(splitWords) == len(set(splitWords)):
+    words = line.split()
+    #Sort the characters within the string. Anagrams will result in identical strings when sorted
+    words = [''.join(sorted(split(word))) for word in words]
+    #print(words)
+    
+    if len(words) == len(set(words)):
         validPasswords += 1
         
 print(validPasswords)
