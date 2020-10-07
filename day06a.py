b7bc79f5ff3ade72866fcab76211abf845b1a680
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-07 15:51:39 -0400

    Day 06a complete

--- day06a.py
+++ day06a.py
@@ -1,7 +1,25 @@
 import fileinput
 
+history = []
+numOps = 0;
+
 indata = fileinput.input().readline()
 banks = [int(num) 
             for num in indata.split()
             ]
-print (banks)
+            
+while banks not in history:
+    history.append(banks.copy())
+    numOps += 1
+    curIndex = banks.index(max(banks)) #find the >lowest-indexed< maximum value
+    curBlocks = banks[curIndex]
+    banks[curIndex] = 0
+    while curBlocks > 0:
+        curIndex += 1
+        curIndex %= len(banks)
+        banks[curIndex] += 1
+        curBlocks -= 1
+    #print (banks)
+    #print (history)
+    
+print (numOps)
