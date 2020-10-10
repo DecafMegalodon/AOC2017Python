b73ea51c9ca2b6113b5d8ce160fedcc53debaaac
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-10 14:17:20 -0400

    Day 09a complete!

--- day09a.py
+++ day09a.py
@@ -2,4 +2,30 @@
 import fileinput
 
 streamData = fileinput.input().readline()
-print(streamData)
+skipping = False
+isGarbage = False
+depth = 0
+score = 0
+
+for char in streamData:
+    if char == '\n':
+        break
+    if skipping:
+        skipping = False
+        continue
+    if isGarbage:
+        if char == '!':
+            skipping = True
+        if char == '>': #Garbage over
+            isGarbage = False
+        continue
+    if char == '<':
+        isGarbage = True
+        continue
+    if char == '{':
+        depth += 1
+    if char == '}':
+        score += depth
+        depth -= 1
+    assert depth >= 0
+print(score)
