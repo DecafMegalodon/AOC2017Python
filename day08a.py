7303db5d383271e39d3710b770b34663647b3e0e
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-10 13:20:47 -0400

    [D08a] Progress is being made, but something is wrong

--- day08a.py
+++ day08a.py
@@ -1,6 +1,13 @@
 #https://adventofcode.com/2017/day/8
 import fileinput
 
+operations = {"<": lambda a, b : a<b,
+">": lambda a, b : a>b,
+">=": lambda a, b : a>=b,
+"<=": lambda a, b : a<=b,
+"==": lambda a, b : a==b,
+"!=": lambda a, b : a!=b}
+
 memory = {}
 maxreg = 0 #The highest value a register has achieved so far
 
@@ -16,12 +23,20 @@ for line in fileinput.input():
     splitLine = line.split()
     targReg = splitLine[0] #The register we -might- modify
     opSign = 1 if splitLine[1] == 'inc' else -1 #Values may only be inc or dec
-    targAmount = splitLine[2] #The amount we by which we might increase/decrease the register
+    targAmount = int(splitLine[2]) #The amount we by which we might increase/decrease the register
+    #splitLine 3 will always be "if"
     opReg = splitLine[4] #The register we're inspecting for the conditional opcode
     opCondition = splitLine[5] #< > >= == <= != are all allowed
-    opTarget = splitLine[6] #The number against which we'll compare opreg
+    opTarget = int(splitLine[6]) #The number against which we'll compare opreg
     
+    opRegValue = readMemory(memory, opReg)
+    targRegValue = readMemory(memory, targReg)
+    #Apparently there's no c-style switch case in python. This looks like a good excuse to learn about lambdas
+    opfunc = operations[opCondition] #Convert the text version of <, >, !=, etc to python code
+    opTrue = opfunc(opRegValue, opTarget)
     
+    if opTrue:
+        memory[targReg] = targRegValue + opSign*targAmount
+        maxreg = max(maxreg, memory[targReg])
     
-    
-    print (splitLine)
+print(maxreg)
