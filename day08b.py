8fff3260e31216275d5bdd9c4483489202c3a30e
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-10 13:47:02 -0400

    [Day 08b] ... Wanted the maximum value ever. Day 08b completed

---
+++ day08b.py
@@ -0,0 +1,42 @@
+#https://adventofcode.com/2017/day/8
+import fileinput
+
+operations = {"<": lambda a, b : a<b,
+">": lambda a, b : a>b,
+">=": lambda a, b : a>=b,
+"<=": lambda a, b : a<=b,
+"==": lambda a, b : a==b,
+"!=": lambda a, b : a!=b}
+
+memory = {}
+maxreg = 0 #The highest value a register has achieved so far
+
+def readMemory(memory, register):
+    if register in memory:
+        return memory[register]
+    else:
+        return 0 #Undefined registered are implied to be 0
+
+for line in fileinput.input():
+    if line == '\n':
+        break
+    splitLine = line.split()
+    targReg = splitLine[0] #The register we -might- modify
+    opSign = 1 if splitLine[1] == 'inc' else -1 #Values may only be inc or dec
+    targAmount = int(splitLine[2]) #The amount we by which we might increase/decrease the register
+    #splitLine 3 will always be "if"
+    opReg = splitLine[4] #The register we're inspecting for the conditional opcode
+    opCondition = splitLine[5] #< > >= == <= != are all allowed
+    opTarget = int(splitLine[6]) #The number against which we'll compare opreg
+    
+    opRegValue = readMemory(memory, opReg)
+    targRegValue = readMemory(memory, targReg)
+    #Apparently there's no c-style switch case in python. This looks like a good excuse to learn about lambdas
+    opfunc = operations[opCondition] #Convert the text version of <, >, !=, etc to python code
+    opTrue = opfunc(opRegValue, opTarget)
+    
+    if opTrue:
+        memory[targReg] = targRegValue + opSign*targAmount
+        maxreg = max(maxreg, memory[targReg])
+    
+print(maxreg)
