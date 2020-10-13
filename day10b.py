e13d3d8839caf53bb01644537fdb165f6a29b848
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-13 13:10:04 -0400

    Day 10b start! Things are starting to look more like a modern hash function

---
+++ day10b.py
@@ -0,0 +1,48 @@
+#https://adventofcode.com/2017/day/10
+import fileinput
+
+"""
+Our input is a list of lengths
+Our "state" is 256 elements long, circular, and 0 indexed
+Lengths larger than the size of the list are invalid.
+
+
+Reverse the order of that length of elements in the list
+    starting with the element at the current position.
+Move the current position forward by that length plus the skip size.
+Increase the skip size by one.
+"""
+
+#Reverses a subrange of a list given a start and length, wrapping around if needed.
+def reverseSpan(rope, start, length):
+    #Technically, we're working with "string" not a "rope",
+        #but rope was chosen as  name here for clarity
+
+    workRope = rope+rope
+    modRope = workRope[start:start+length][::-1]
+    
+    for i in range(0,length):
+        workRope[(start+i)%len(rope)] = modRope[i]
+    workRope = workRope[0:len(rope)]
+    
+    return workRope
+
+indata = fileinput.input().readline()
+data = [int(num) 
+            for num in indata.split(',')
+            ]
+
+rope = [i for i in range(0,256)]
+
+
+curPos = 0
+skip = 0
+for length in data:
+    rope = reverseSpan(rope, curPos, length)
+    curPos += length
+    curPos += skip
+    curPos %= len(rope)
+    skip += 1
+    
+print(rope[0]*rope[1])
+#22052 is too high
