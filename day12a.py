3104c9214d8ccc00ccfa1ec51f29db0b000b4c04
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-16 17:52:51 -0400

    [D12a] Completed! Not pretty, but done fast and right

--- day12a.py
+++ day12a.py
@@ -1,7 +1,7 @@
 #https://adventofcode.com/2017/day/12
 import fileinput
 
-network = {0} #0 is implicitly connected to itself
+network = {'0'} #0 is implicitly connected to itself
 unconnecteds = {} #Nodes we haven't yet determined if they're connected to 0 or not
 
 for line in fileinput.input():
@@ -11,4 +11,14 @@ for line in fileinput.input():
     children = [address.strip(",") for address in splitline[2::]]
     unconnecteds[splitline[0]] = children
     
-print (unconnecteds)
+changed = True
+
+while changed:
+    changed = False
+    for con in unconnecteds:
+        if con in network:
+            changed = True
+            network.update(unconnecteds[con])
+            del unconnecteds[con]
+            break
+print(len(network))
