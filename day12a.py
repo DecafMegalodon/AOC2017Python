cb0af48f6368de84246a6f3823a00241b93a4fad
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-16 17:43:56 -0400

    [D12a] Basic input completed

--- day12a.py
+++ day12a.py
@@ -1,2 +1,14 @@
 #https://adventofcode.com/2017/day/12
 import fileinput
+
+network = {0} #0 is implicitly connected to itself
+unconnecteds = {} #Nodes we haven't yet determined if they're connected to 0 or not
+
+for line in fileinput.input():
+    if line == '\n':
+        break
+    splitline = line.split()
+    children = [address.strip(",") for address in splitline[2::]]
+    unconnecteds[splitline[0]] = children
+    
+print (unconnecteds)
