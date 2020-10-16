457019b471ac7f893ed80b874acfc27ba10b2a18
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-16 16:06:47 -0400

    [D11a] Complete after some mathematical soul searching

--- day11a.py
+++ day11a.py
@@ -4,8 +4,8 @@ from collections import Counter
 
 """
 Equivalences:
-NW+S = SW, SW+N = NW
-NE+S = SE, SE+N = NE
+NW+S = SW, SE+N = NE
+NE+S = SE, SW+N = NW
 SE+SW = S, NE+NW = N
 N+S = 0, NE+SW = 0, NW+SE = 0
 
@@ -14,21 +14,51 @@ It should be possible to make a two-dimensional coordinate plane with a nw-se (o
     and a n/s axis
 Combined with the equivalences up above, it may be possible to quickly reduce the counts to distances
 """
+
 path = fileinput.input().readline()[:-1:].split(',') #Trim out newline at the end and split up!
 pathCounts = Counter(path)
+
+changed = True
+
+
+#N+S -> 0, NE+SW -> 0, NW+SE -> 0
 dist1 = pathCounts['nw'] - pathCounts['se']
 dist2 = pathCounts['ne'] - pathCounts['sw']
 dist3 = pathCounts['n'] - pathCounts['s']
-equivPath = {'nw': dist1 if dist1 > 0 else 0,
-    'se': abs(dist1), 
-    'n': 0, 
-    's': 0, 
-    'ne':0, 
-    'sw':0}
-if(dist1 > 0):
-    equivPath['nw'] = dist1
-else:
-    equivPath['se'] = abs(dist1)
-print(dist1, dist2, dist3)
-print(dist1 + dist2 + dist3)
+
+if (dist1 * dist3) < 0: #If they're opposite signs and both non-zero. Check for NW/SE + S/N -> SW/NW indentity
+    delta = min(abs(dist1),abs(dist3))
+    if dist1 > dist3: #NW+S -> SW
+        dist2 -= delta
+        dist1 -= delta
+        dist3 += delta
+    else: #SE+N -> NE
+        dist2 += delta
+        dist1 += delta
+        dist3 -= delta
+
+#Copy paste code BAD.        
+if dist2 * dist3 < 0: #NE/SW + S/N -> SE/NW
+    delta = min(abs(dist2), abs(dist3))
+    if dist2 > dist3: #NE + S -> SE
+        dist2 -= delta
+        dist1 -= delta
+        dist3 += delta
+    else: #SW+N -> NW
+        dist2 += delta
+        dist1 += delta
+        dist3 -= delta
+        
+if dist1 * dist2 > 0: #SE+SW = S, NE+NW = N
+    delta = min(abs(dist1), abs(dist2))
+    if dist1 > 0: #NE+NW = N
+        dist2 -= delta
+        dist1 -= delta
+        dist3 += delta
+    else: #SE+SW = S
+        dist2 += delta
+        dist1 += delta
+        dist3 -= delta
+    
+print(abs(dist1) + abs(dist2) + abs(dist3))
 
