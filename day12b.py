8c5c0163a3d12f4927f021aba4425919bc5f5d20
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-16 18:07:20 -0400

    [D12b] Complete!

--- day12b.py
+++ day12b.py
@@ -1,7 +1,6 @@
 #https://adventofcode.com/2017/day/12
 import fileinput
 
-network = {'0'} #0 is implicitly connected to itself
 unconnecteds = {} #Nodes we haven't yet determined if they're connected to 0 or not
 
 for line in fileinput.input():
@@ -11,14 +10,21 @@ for line in fileinput.input():
     children = [address.strip(",") for address in splitline[2::]]
     unconnecteds[splitline[0]] = children
     
-changed = True
+numNetworks = 0
 
-while changed:
-    changed = False
-    for con in unconnecteds:
-        if con in network:
-            changed = True
-            network.update(unconnecteds[con]) #Add the children to the reachable list
-            del unconnecteds[con]
-            break
-print(len(network))
+while len(unconnecteds) > 0:
+    numNetworks += 1
+    node = unconnecteds.popitem() #Fetch an item... any item.
+    network = {node[0]} #And we'll see what connects to it
+    network.update(node[1])
+    
+    changed = True
+    while changed:
+        changed = False
+        for con in unconnecteds:
+            if con in network:
+                changed = True
+                network.update(unconnecteds[con]) #Add the children to the reachable list
+                del unconnecteds[con]
+                break
+print(numNetworks)
