8d968e8cdfd7b8481ba2fdd69939496ec1f521ff
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-17 17:45:47 -0400

    [D13b] Refactor severity calculation into its own function

--- day13b.py
+++ day13b.py
@@ -1,5 +1,6 @@
 #https://adventofcode.com/2017/day/13
 import fileinput
+import copy
 
 firewall = {}
 severity = 0
@@ -22,10 +23,15 @@ def advanceFirewall(firewall):
         elif fwobject['position'] == 0:
             fwobject['direction'] = 1
 
-for i in range(0,maxdepth+1):
-    if str(i) in firewall:
-        if firewall[str(i)]["position"] == 0:
-            #print("Caught at", i)
-            severity += i * firewall[str(i)]["range"]
-    advanceFirewall(firewall)
-print(severity)
+def calcSeverity(OGFirewall, maxdepth):
+    firewall = copy.deepcopy(OGFirewall)
+    severity = 0
+    for i in range(0,maxdepth+1):
+        if str(i) in firewall:
+            if firewall[str(i)]["position"] == 0:
+                #print("Caught at", i)
+                severity += i * firewall[str(i)]["range"]
+        advanceFirewall(firewall)
+    return severity
+    
+print(calcSeverity(firewall,maxdepth))
