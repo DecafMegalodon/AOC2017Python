df211ac1e89fa529f2e9f546a6fdc09ff25749fa
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-17 16:13:11 -0400

    [D13b] Starting

---
+++ day13b.py
@@ -0,0 +1,31 @@
+#https://adventofcode.com/2017/day/13
+import fileinput
+
+firewall = {}
+severity = 0
+maxdepth = -1
+curdepth = -1
+
+for line in fileinput.input():
+    if line == '\n':
+        break
+    splitline = line.split()
+    firewall[splitline[0].strip(':')] = {'position': 0, 'range': int(splitline[1]), 'direction': 1}
+    maxdepth = max(maxdepth, int(splitline[0].strip(':')))
+
+def advanceFirewall(firewall):
+    for depth in firewall:
+        fwobject = firewall[depth]
+        fwobject['position'] += fwobject['direction']
+        if fwobject['position'] == fwobject['range']-1:
+            fwobject['direction'] = -1
+        elif fwobject['position'] == 0:
+            fwobject['direction'] = 1
+
+for i in range(0,maxdepth+1):
+    if str(i) in firewall:
+        if firewall[str(i)]["position"] == 0:
+            #print("Caught at", i)
+            severity += i * firewall[str(i)]["range"]
+    advanceFirewall(firewall)
+print(severity)
