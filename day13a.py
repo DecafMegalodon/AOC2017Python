cddccc83560cff820d88ad8aee6220d3ee4735e9
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-17 14:44:23 -0400

    [D13a] Basic input completed

--- day13a.py
+++ day13a.py
@@ -1,1 +1,12 @@
 #https://adventofcode.com/2017/day/13
+import fileinput
+
+firewall = {}
+severity = 0
+
+for line in fileinput.input():
+    if line == '\n':
+        break
+    splitline = line.split()
+    firewall[splitline[0].strip(':')] = {'position': 0, 'range': int(splitline[1]), 'direction': 'down'}
+print(firewall)
