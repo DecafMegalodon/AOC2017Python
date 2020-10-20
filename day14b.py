fb0bddff003e454b4c402c5eac20174c2ac68f16
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-20 12:29:03 -0400

    [D14b] Almost there. Just a smidge of debugging left

--- day14b.py
+++ day14b.py
@@ -16,14 +16,38 @@ def setDiskOccuptation(disk, y, x, occupied):
     if x > 127 or y > 127 or x < 0 or y < 0:
         raise "NotOnDiskError y%d x%d" % (y, x)
     else:
-        return disk[y*128 + x] = occupied
+        disk[y*128 + x] = occupied
+        
+"""Destructively finds the area of a contiguous block of data"""
+def recurseAreaFind(disk, y, x):
+    if getDiskOccupation(disk, y, x) == 0:
+        return 0
 
+    area = 1
+    setDiskOccuptation(disk, y, x, 0)
+    for yoff in [-1, 1]:
+        for xoff in [-1, 1]:
+            area += recurseAreaFind(disk, y+yoff, x+xoff)
+
+    return area
+
+#Build disk bitmap
 for append in range(0,128):
     section = 0
     knothash = calcKnotHash(puzInput + '-' + str(append))
     for bits8 in knothash:
         for bit in range(7,-1,-1):
-            thisBit = bits8&(1<<bit)>0
-            setDiskOccuptation(disk,append,section*8 + 7 - bit, thisBit)
+            thisBit = bits8&(1<<bit)>0  # True == 1
+            setDiskOccuptation(fullDisk,append,section*8 + 7 - bit, thisBit)
         section += 1
-print("SUM", sum(fullDisk))
+
+
+numRegions = 0
+
+for y in range(0,128):
+    for x in range(0,128):
+        if recurseAreaFind(fullDisk, y, x) > 0:
+            numRegions += 1
+            
+print(numRegions)
+#print(fullDisk)
