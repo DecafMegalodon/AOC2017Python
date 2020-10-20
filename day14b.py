a391a12645cf6c9214fa6c2180769bce377b1685
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-20 11:36:34 -0400

    [D14b] Generate the actual disk "bitmap"

--- day14b.py
+++ day14b.py
@@ -3,10 +3,16 @@ from day10knothash import *
 
 puzInput = fileinput.input().readline()[:-1:]  # Omit the trailing newline
 
-fullDisk = []
+fullDisk = [0]*128*128
 
 for append in range(0,128):
+    section = 0
     knothash = calcKnotHash(puzInput + '-' + str(append))
     for bits8 in knothash:
-        utilized += sum([bits8&(1<<loc)>0 for loc in range(8)])
-    #print(printableHash(knothash))
+        for bit in range(7,-1,-1):
+            index = append*128 + section*8 + (7-bit)
+            print(index)
+            fullDisk[index] = bits8&(1<<bit)>0
+            #fullDisk[index] = 1
+        section += 1
+print("SUM", sum(fullDisk))
