ad55f15f8e67cdb073ee079568631fd0c2c17fe8
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-20 10:51:00 -0400

    [D14a] Complete thanks to some bitwise "magic"

--- day14a.py
+++ day14a.py
@@ -2,3 +2,13 @@ import fileinput
 from day10knothash import *
 
 puzInput = fileinput.input().readline()[:-1:]  # Omit the trailing newline
+
+fullDisk = []
+utilized = 0
+
+for append in range(0,128):
+    knothash = calcKnotHash(puzInput + '-' + str(append))
+    for bits8 in knothash:
+        utilized += sum([bits8&(1<<loc)>0 for loc in range(8)])
+    #print(printableHash(knothash))
+print(utilized)
