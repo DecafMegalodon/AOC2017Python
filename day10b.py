90da7bf1b066823ff86cd1759a479e207d864bcd
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-20 10:00:12 -0400

    [D10b] Some code standardization while I'm here for D14a

--- day10b.py
+++ day10b.py
@@ -1,4 +1,4 @@
-#https://adventofcode.com/2017/day/10
+# https://adventofcode.com/2017/day/10
 import fileinput
 
 """
@@ -14,15 +14,17 @@ Increase the skip size by one.
 """
 
 
-#Reverses a subrange of a list given a start and length, wrapping around if needed.
+"""
+# Reverses a subrange of a list given a start and length, wrapping around if needed.
+# Technically, we're working with "string" not a "rope",
+# but rope was chosen as  name here for clarity
+"""
 def reverseSpan(rope, start, length):
-    #Technically, we're working with "string" not a "rope",
-        #but rope was chosen as  name here for clarity
     workRope = rope+rope
     modRope = workRope[start:start+length][::-1]
-    
-    for i in range(0,length):
-        workRope[(start+i)%len(rope)] = modRope[i]
+
+    for i in range(0, length):
+        workRope[(start+i) % len(rope)] = modRope[i]
     workRope = workRope[0:len(rope)]
     
     return workRope
@@ -32,7 +34,7 @@ def printableHash(denseHash):
     fullHash = ""
     for eightbits in denseHash:
         partHash = hex(eightbits)
-        partHash = partHash[2::] #Trim off the 0x
+        partHash = partHash[2::]  # Trim off the 0x
         if(len(partHash) == 1):
             partHash = '0' + partHash
         fullHash += partHash
@@ -41,15 +43,15 @@ def printableHash(denseHash):
 
 """Takes a string input and returns it back as a knothash in a list, 8 bits at a time"""
 def calcKnotHash(datastring):
-    rope = [i for i in range(0,256)]
+    rope = [i for i in range(0, 256)]
     denseHash = []
     curPos = 0
     skip = 0
     
     data = [ord(char) for char in datastring]
-    data += [17,31,73,47,23]
+    data += [17, 31, 73, 47, 23]
     
-    for round in range(0,64):
+    for round in range(0, 64):
         for length in data:
             rope = reverseSpan(rope, curPos, length)
             curPos += length
@@ -57,15 +59,14 @@ def calcKnotHash(datastring):
             curPos %= len(rope)
             skip += 1
             
-    for block in range(0,16):
+    for block in range(0, 16):
         blockData = rope[block*16]
-        for subblock in range(1,16):
+        for subblock in range(1, 16):
             blockData = blockData ^ rope[block*16+subblock]
         denseHash.append(blockData)
-    
     return denseHash
 
 
-indata = fileinput.input().readline()[:-1:] #Omit the trailing newline python adds
+indata = fileinput.input().readline()[:-1:]  # Omit the trailing newline python adds
 knothash = calcKnotHash(indata)
 print(printableHash(knothash))
