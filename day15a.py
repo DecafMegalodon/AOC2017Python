76661960cbf5f6473eab8035a9c62fee73a89ea9
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-22 11:00:04 -0400

    [D15a] Complete!

--- day15a.py
+++ day15a.py
@@ -5,5 +5,15 @@ import fileinput
 stdin = fileinput.input()
 genA = int(stdin.readline().split()[4])
 genB = int(stdin.readline().split()[4])
+genAFactor = 16807
+genBFactor = 48271  # Both of these are provided by the puzzle outside of input
 
-print(genA, genB)
+agreement = 0
+
+for round in range(40000000):
+    genA = (genA*genAFactor) % 2147483647
+    genB = (genB*genBFactor) % 2147483647
+    if genA % 65536 == genB % 65536:
+        agreement += 1
+        
+print(agreement)
