1883df1f5681f23b3fc6ab41e6c6513d648fcb70
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-24 19:19:19 -0400

    [D17a] More work on circular buffer

--- day17a.py
+++ day17a.py
@@ -2,16 +2,14 @@
 import fileinput
 
 class LLnode:
-    def __init__(self, data):
-        self.data = data
-        self.nextnode = None
-    def __init__(self, data, nextnode):
+    def __init__(self, data, nextnode = None):
         self.data = data
         self.nextnode = nextnode
         
-class LinkedList:
-    def __ini__(self):
+class CircLinkedList:
+    def __init__(self):
         self.head = None
+        self.size = 0
 
     '''Inserts the data in a new node in front of the current head'''
     def insert(self, data):
@@ -20,13 +18,26 @@ class LinkedList:
         else:
             self.head = LLnode(data)
             self.head.nextnode = self.head
+        self.size += 1
 
     def advance(self):
         if self.head != None:
             self.head = self.head.nextnode
             
     def __str__(self):
-        return "cats"
+        if self.head ==  None:
+            return "None"
+        else:
+            origNode = self.head
+            curNode = origNode.nextnode
+            strRep = ">" + str(origNode.data) + "<  -> "
+            while curNode != origNode:
+                strRep += str(curNode.data) + " -> "
+                curNode = curNode.nextnode
+            return strRep
         
-ll = LinkedList()
+ll = CircLinkedList()
+ll.insert("Moo")
+ll.insert("Boo")
+ll.insert("Voodoo")
 print(ll)
