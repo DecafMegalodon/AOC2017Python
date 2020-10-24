b2bdc846f92dc67c7af0b7a3cf4e02ae72f26f17
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-24 19:37:59 -0400

    [D17a] Complete :)

--- day17a.py
+++ day17a.py
@@ -20,9 +20,19 @@ class CircLinkedList:
             self.head.nextnode = self.head
         self.size += 1
 
-    def advance(self):
+    def advance(self, amount = 1):
         if self.head != None:
-            self.head = self.head.nextnode
+            for i in range(amount, 0, -1):
+                self.head = self.head.nextnode
+                
+    '''Seeks the circular LL to the 'first' instance of the target after the current index, if any.
+        Does nothing if it's not found'''
+    def seek(self, target):
+        if self.head != None:
+            curNode = self.head.nextnode
+            while curNode.data != target and curNode != self.head:
+                curNode = curNode.nextnode
+            self.head = curNode
             
     def __str__(self):
         if self.head ==  None:
@@ -30,14 +40,18 @@ class CircLinkedList:
         else:
             origNode = self.head
             curNode = origNode.nextnode
-            strRep = ">" + str(origNode.data) + "<  -> "
+            strRep = ">" + str(origNode.data) + "< -> "
             while curNode != origNode:
                 strRep += str(curNode.data) + " -> "
                 curNode = curNode.nextnode
             return strRep
         
 ll = CircLinkedList()
-ll.insert("Moo")
-ll.insert("Boo")
-ll.insert("Voodoo")
-print(ll)
+puzInput = int(fileinput.input().readline().strip("\n"))
+
+for i in range(2017+1):
+    ll.insert(i)
+    ll.advance(1 + puzInput)
+ll.seek(2017)
+ll.advance()
+print(ll.head.data)
