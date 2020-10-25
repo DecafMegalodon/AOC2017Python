61423017b8d51c89b8acc68736846cc79ab0fc81
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-24 20:37:16 -0400

    [D17b] Technically complete, but terrible and slow. > 9GB of RAM and counting

---
+++ day17b.py
@@ -0,0 +1,57 @@
+#  https://adventofcode.com/2017/day/16
+import fileinput
+
+class LLnode:
+    def __init__(self, data, nextnode = None):
+        self.data = data
+        self.nextnode = nextnode
+        
+class CircLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    '''Inserts the data in a new node in front of the current head'''
+    def insert(self, data):
+        if self.head != None:
+            self.head.nextnode = LLnode(data, self.head.nextnode)
+        else:
+            self.head = LLnode(data)
+            self.head.nextnode = self.head
+        self.size += 1
+
+    def advance(self, amount = 1):
+        if self.head != None:
+            for i in xrange(amount, 0, -1):
+                self.head = self.head.nextnode
+                
+    '''Seeks the circular LL to the 'first' instance of the target after the current index, if any.
+        Does nothing if it's not found'''
+    def seek(self, target):
+        if self.head != None:
+            curNode = self.head.nextnode
+            while curNode.data != target and curNode != self.head:
+                curNode = curNode.nextnode
+            self.head = curNode
+            
+    def __str__(self):
+        if self.head ==  None:
+            return "None"
+        else:
+            origNode = self.head
+            curNode = origNode.nextnode
+            strRep = ">" + str(origNode.data) + "< -> "
+            while curNode != origNode:
+                strRep += str(curNode.data) + " -> "
+                curNode = curNode.nextnode
+            return strRep
+        
+ll = CircLinkedList()
+puzInput = int(fileinput.input().readline().strip("\n"))
+
+for i in range(50000000+1):
+    ll.insert(i)
+    ll.advance(1 + puzInput)
+ll.seek(0)
+ll.advance()
+print(ll.head.data)
