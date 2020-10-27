c9215507a2578e4391ab9991b64dfb98b7abe78a
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-27 18:56:17 -0400

    [D18a] Skeleton of opcodes

--- day18a.py
+++ day18a.py
@@ -1,6 +1,26 @@
 #  https://adventofcode.com/2017/day/18
 import fileinput
 
+registers = {}
+for regName in range(ord("a"),ord("z")+1):
+    registers[chr(regName)] = 0
+
 for instruction in fileinput.input():
     splitline = instruction.split()
-    print(splitline)
+    op = splitline[0]
+    if op == 'snd':
+        continue
+    elif op == 'set':
+        continue
+    elif op == 'add':
+        continue
+    elif op == 'mul':
+        continue
+    elif op == 'mod':
+        continue
+    elif op == 'rcv':
+        continue
+    elif op == 'jgz':
+        continue
+    
+    
