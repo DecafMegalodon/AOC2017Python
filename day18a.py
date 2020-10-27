3fc888f41531950679706ea33958bb953de9b309
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-27 19:27:28 -0400

    [D18a] Memory read/write abstraction

--- day18a.py
+++ day18a.py
@@ -4,13 +4,26 @@ import fileinput
 registers = {}
 for regName in range(ord("a"),ord("z")+1):
     registers[chr(regName)] = 0
-    
+
+def readMem(memory, target):
+    try: #Is target a number?
+        return int(target)
+    except: #target is not a number
+        return memory[target]
+        
+def writeMem(memory, target, value):
+    memory[target] = readMem(memory, value)
+        
 program = [line.split() for line in fileinput.input]
 pc = 0
+lastSound = None
 
 while True:
     instruction = program[pc]
     op = instruction[0]
+    v1 = instruction[1]
+    v2 = instruction[2] if len(instruction) > 2 else None
+        
     if op == 'snd':
         continue
     elif op == 'set':
