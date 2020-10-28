fb2cb0bd9cc628d0fe36f6e7b996f20236352e36
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-28 14:45:47 -0400

    [D18b] Attempt 1, but not quite it seems

--- day18b.py
+++ day18b.py
@@ -11,48 +11,72 @@ class Program:
         self.registers = {}
         for regName in range(ord("a"),ord("z")+1):
             self.registers[chr(regName)] = 0
+        self.registers['p'] = ID
         self.sendQueue = []
         self.numSent = 0
+        self.program = initProgram
 
     def readMem(self, target):
+        #print("Trying to read: ", target)
         try: #Is target a number?
             return int(target)
         except: #target is not a number
             return self.registers[target]
             
     def writeMem(self, target, value):
-        self.registers[target] = self.readMem(registers, value)
+        self.registers[target] = self.readMem(value)
         
-    def run(self):
-        continue
+    '''Runs the program in memory until it halts for some reason
+       Returns the number of instructions run for this invocation of run
+    '''
+    def run(self, otherComputer):
+        numInstructionsRunThisWake = 0
+        
+        while True:
+        
+            if self.pc >= len(self.program): #Reached the end of prog
+                return numInstructionsRunThisWake
+            instruction = self.program[self.pc]
+            op = instruction[0]
+            v1 = instruction[1]
+            v2 = instruction[2] if len(instruction) > 2 else None
+            
+            print(self.registers['p'], instruction)
+            
+            if op == 'snd':
+                    self.sendQueue.append(self.readMem(v1))
+                    self.numSent += 1
+            elif op == 'set':
+                self.writeMem(v1, v2)
+            elif op == 'add':
+                self.writeMem(v1, 
+                    self.readMem(v1) + self.readMem(v2))
+            elif op == 'mul':
+                self.writeMem(v1, 
+                    self.readMem(v1) * self.readMem(v2))
+            elif op == 'mod':
+                self.writeMem(v1, 
+                    self.readMem(v1) % self.readMem(v2))
+            elif op == 'rcv':
+                if len(otherComputer.sendQueue) == 0:
+                    return numInstructionsRunThisWake
+                else:
+                    self.writeMem(v1, otherComputer.sendQueue.pop(0))
+            elif op == 'jgz':
+                if self.readMem(v1) > 0:
+                    self.pc += self.readMem(v2)
+                    numInstructionsRunThisWake += 1
+                    continue #  Skip the PC increase
+            self.pc += 1
+            numInstructionsRunThisWake += 1
 
-while True:
-    instruction = program[pc]
-    op = instruction[0]
-    v1 = instruction[1]
-    v2 = instruction[2] if len(instruction) > 2 else None
 
-    #print(instruction)
-    if op == 'snd':
-        lastSound = readMem(registers, v1)
-    elif op == 'set':
-        writeMem(registers, v1, v2)
-    elif op == 'add':
-        writeMem(registers, v1, 
-            readMem(registers, v1) + readMem(registers, v2))
-    elif op == 'mul':
-        writeMem(registers, v1, 
-            readMem(registers, v1) * readMem(registers, v2))
-    elif op == 'mod':
-        writeMem(registers, v1, 
-            readMem(registers, v1) % readMem(registers, v2))
-    elif op == 'rcv':
-        if readMem(registers, v1) != 0:
-            print(lastSound)
-            exit(0)
-    elif op == 'jgz':
-        if readMem(registers, v1) > 0:
-            pc += readMem(registers, v2)
-            continue #  Skip the PC increase
-    
-    pc += 1
+prog1 = Program(program, 0)
+prog2 = Program(program, 1)
+
+#  While at least one instruction is executed between the two:
+while(prog1.run(prog2) + prog2.run(prog1) > 0):
+    continue
+
+print(prog1.numSent)
+#6096 too high
