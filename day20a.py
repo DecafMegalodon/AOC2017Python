5d3d2462bb1a3ab628e4c465a7a99aa65b9f6d3d
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-10-30 13:31:15 -0400

    [D20a] Implemented a data class for particles and some functions for it

--- day20a.py
+++ day20a.py
@@ -1,2 +1,19 @@
 #  https://adventofcode.com/2017/day/20
 import fileinput
+
+class particle:
+    def __init__(self, pos, vel, acc):
+        self.pos = pos
+        self.vel = vel
+        self.acc = acc
+        self.dist = self.distance()
+        
+    def distance(self):
+        return abs(self.pos[0]) + abs(self.pos[1]) + abs(self.pos[2])
+        
+
+    def __lt__(self, other):
+        return self.distance() < other.distance()
+        
+speck = particle([1,1,1], [], [])
+print(speck.dist)
