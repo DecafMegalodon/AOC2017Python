887fe32231e95e4e3b110aef741d5cfc8f396da4
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-02 09:33:38 -0500

    [D21a] Implement rotation

--- day21a.py
+++ day21a.py
@@ -1,3 +1,31 @@
 #  https://adventofcode.com/2017/day/21
 import fileinput
 
+
+'''
+https://stackoverflow.com/questions/8421337/rotating-a-two-dimensional-array-in-python
+served as inspiration for this implementation
+
+It takes in a "matrix" and transforms it a few times
+E.G. Take the follow matrix:
+
+           \/-Performs a vertical flip
+[['..#'] [::-1] ['###'] zip   [#..](As serpate characters)
+ ['.#.']  --->  ['.#.'] --->  [##.]
+ ['###']        ['..#']  /\   [#.#]
+              Mirrors across the x=y axis
+              
+The vertical flip plus the mirror across the x=y axis is equivalent to a 90 degree rotation
+
+Rotates a subimage clockwise (with row major ordering)'''
+def rotate(subimg, times=1):
+    rotated = subimg
+    for i in range(times):
+        rotated = list(zip(*subimg[::-1]))
+    return [''.join(j) for j in rotated]
+    
+
+test = ['###','...','...']
+for i in range(4):
+    test = rotate(test)
+    print(test, '\n')
