d2ab0fde8794d57e61dbfe2cfb298ae9d8ff0f20
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-02 09:47:46 -0500

    [D21a] Implemented horizontal and vertical flipping

--- day21a.py
+++ day21a.py
@@ -16,16 +16,27 @@ E.G. Take the follow matrix:
               Mirrors across the x=y axis
               
 The vertical flip plus the mirror across the x=y axis is equivalent to a 90 degree rotation
+A counterclockwise flip could be achieved by doing the flips in reverse order
 
 Rotates a subimage clockwise (with row major ordering)'''
 def rotate(subimg, times=1):
     rotated = subimg
-    for i in range(times):
-        rotated = list(zip(*subimg[::-1]))
+    for i in range(0, times):
+        rotated = list(zip(*rotated[::-1]))
     return [''.join(j) for j in rotated]
     
 
-test = ['###','...','...']
-for i in range(4):
-    test = rotate(test)
-    print(test, '\n')
+#Returns a shallow-copied vertical flip of the given image
+def flipVert(subimg):
+    return subimg[::-1]
+    
+#Returns a slightly less shallow-copied horizonal flip of the given image
+def flipHoriz(subimg):
+    return [i[::-1] for i in subimg]
+    
+    
+test = ['#..','...','x..']
+test = flipVert(test)
+test = flipHoriz(test)
+test = rotate(test, 2)
+print(test)
