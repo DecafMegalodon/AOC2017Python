03d0bfb77188125ac37b5c49a8a17217029bea41
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-07 10:01:07 -0500

    [D23a] Completed without any fuss

--- day23a.py
+++ day23a.py
@@ -2,19 +2,14 @@
 import fileinput
         
 program = [line.split() for line in fileinput.input()]
-pc = 0
-lastSound = None
 
 class Program:
     def __init__(self, initProgram, ID):
         self.pc = 0 #program counter
         self.registers = {}
-        for regName in range(ord("a"),ord("z")+1):
+        for regName in range(ord("a"),ord("h")+1):
             self.registers[chr(regName)] = 0
-        self.registers['p'] = ID
-        self.sendQueue = []
-        self.numSent = 0
-        self.program = initProgram
+        self.program = initProgram #  Shallow copied
 
     def readMem(self, target):
         #print("Trying to read: ", target)
@@ -29,17 +24,18 @@ class Program:
     '''Runs the program in memory until it halts for some reason
        Returns the number of instructions run for this invocation of run
     '''
-    def run(self, otherComputer):
-        numInstructionsRunThisWake = 0
+    def run(self):
+        numMultsThisWake = 0
         
         while True:
         
             if self.pc >= len(self.program): #  Reached the end of prog
-                return numInstructionsRunThisWake
+                return numMultsThisWake
             instruction = self.program[self.pc]
             op = instruction[0]
             v1 = instruction[1]
             v2 = instruction[2] if len(instruction) > 2 else None
+            print(instruction)
             
             if op == 'snd':
                     self.sendQueue.append(self.readMem(v1))
@@ -49,31 +45,30 @@ class Program:
             elif op == 'add':
                 self.writeMem(v1, 
                     self.readMem(v1) + self.readMem(v2))
+            elif op == 'sub':
+                self.writeMem(v1, 
+                    self.readMem(v1) - self.readMem(v2))
             elif op == 'mul':
                 self.writeMem(v1, 
                     self.readMem(v1) * self.readMem(v2))
+                numMultsThisWake += 1
             elif op == 'mod':
                 self.writeMem(v1, 
                     self.readMem(v1) % self.readMem(v2))
-            elif op == 'rcv':
-                if len(otherComputer.sendQueue) == 0:
-                    return numInstructionsRunThisWake
-                else:
-                    self.writeMem(v1, otherComputer.sendQueue.pop(0))
             elif op == 'jgz':
                 if self.readMem(v1) > 0:
                     self.pc += self.readMem(v2)
-                    numInstructionsRunThisWake += 1
                     continue #  Skip the PC increase
+            elif op == 'jnz':
+                if self.readMem(v1) != 0:
+                    self.pc += self.readMem(v2)
+                    continue #  Skip the PC increase
+            else:
+                print('INSTRUCTION NOT FOUND!!!')
             self.pc += 1
-            numInstructionsRunThisWake += 1
 
 
 prog0 = Program(program, 0)
-prog1 = Program(program, 1)
+numMults = prog0.run()
 
-#  While at least one instruction is executed between the two:
-while(prog0.run(prog1) + prog1.run(prog0) > 0):
-    continue
-
-print(prog1.numSent)
+print(numMults)
