e4e78db360bfe07f676526ab16a7195547ed845f
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-09 11:06:44 -0500

    [D24a] Basic storage for connectors

--- day24a.py
+++ day24a.py
@@ -3,7 +3,32 @@ import fileinput
 
 '''Battleplan:
 Data classes for connectors and bridges
+    Can request iterator for matching unused connectors
+    Ideally with otimized lookup
 Recursively build all possible bridges and store them for later inspection
 Sort the bridges by strength
 Reach the CPU over the void!
 '''
+
+
+class bridgeComps:
+    
+    def __init__(self):
+        self._pinLookupDict = {}
+        self._pinTable = []
+        
+    def addConnector(self, pins1, pins2):
+        newIndex = len(self._pinTable)
+        self._pinTable.append((pins1, pins2, 0))
+        for pin in (pins1, pins2):
+            
+            try:
+                self._pinLookupDict[pin].append(newIndex)
+            except:
+                self._pinLookupDict[pin] = [newIndex]
+
+bridgeBits = bridgeComps()
+bridgeBits.addConnector(1,4)
+bridgeBits.addConnector(1,5)
+print(bridgeBits._pinTable)
+print(bridgeBits._pinLookupDict)
