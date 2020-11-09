faf880e44b588e6ae264656f61cb72191ae108ed
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-09 09:27:39 -0500

    [D24a] Starting the penultimate day from 2017!

---
+++ day24a.py
@@ -0,0 +1,9 @@
+#  https://adventofcode.com/2017/day/24
+import fileinput
+
+'''Battleplan:
+Data classes for connectors and bridges
+Recursively build all possible bridges and store them for later inspection
+Sort the bridges by strength
+Reach the CPU over the void!
+'''
