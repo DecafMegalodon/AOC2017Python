2a7cbfe2f07bf230f37a8cab440905c86ae8095f
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-09 11:35:06 -0500

    [D24a] Add lookup for matching pins

--- day24a.py
+++ day24a.py
@@ -19,7 +19,7 @@ class bridgeComps:
         
     def addConnector(self, pins1, pins2):
         newIndex = len(self._pinTable)
-        self._pinTable.append((pins1, pins2, 0))
+        self._pinTable.append([pins1 + pins2, 0])
         for pin in (pins1, pins2):
             
             try:
@@ -27,8 +27,21 @@ class bridgeComps:
             except:
                 self._pinLookupDict[pin] = [newIndex]
 
+    def getMatchingPins(self, hostPins):
+        try:
+            for match in self._pinLookupDict[hostPins]:
+                if self._pinTable[match][1] == 0: #  If it hasn't been used yet
+                    self._pinTable[match][1] = 1 #  Mark as used
+                    yield self._pinTable[match][0] - hostPins
+                    self._pinTable[match][1] = 1 #  Mark as no longer used
+        except: #  No pins matching our hostPins
+            return
+
 bridgeBits = bridgeComps()
 bridgeBits.addConnector(1,4)
 bridgeBits.addConnector(1,5)
-print(bridgeBits._pinTable)
-print(bridgeBits._pinLookupDict)
+bridgeBits.addConnector(4,3)
+for match in bridgeBits.getMatchingPins(4):
+    print(match)
+for match in bridgeBits.getMatchingPins(1000000):
+    print(match)
