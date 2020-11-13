8dfc33b608fdaa14745189accfb380c49d324dd9
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-13 01:00:12 -0500

    [D03b] It's time to (re)visit an old problem and see how much I've grown

--- day03b.py
+++ day03b.py
@@ -7,3 +7,32 @@ Write spiral number generator
 Search for the answer
 """
 import fileinput
+
+class expandGrid:
+    def __init__(self):
+        self.grid = {}
+        
+    def read(self, y, x):
+        try:
+            return self.grid[(y, x)]
+        except:
+            return 0
+            
+    def sumNeighbors(self, y, x):
+        sum = 0
+        for yval in range(y-1, y+2): #  Range is not inclusive of last number
+            for xval in range(x-1, y+2):
+                sum += self.read(yval, xval)
+        return sum
+        
+    def write(self, y, x, value):
+        self.grid[(y,x)] = value
+        
+    def __str__(self):
+        return str(self.grid)
+        
+grid = expandGrid()
+grid.write(0,0,1)
+grid.write(1,1,2)
+grid.write(2,2,1000000000)
+print(grid.sumNeighbors(0,0))
