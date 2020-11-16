f35c4f9c89fa815101ea90582c1c022a67ad226f
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-16 17:26:58 -0500

    [D01A/B] PEP8 standardization

--- day01b.py
+++ day01b.py
@@ -1,15 +1,19 @@
+#  https://adventofcode.com/2017/day/1
 import fileinput
 
+
+#  Return the integer representation of the pos-th digit,
+#  wrapping around to the start on an overflow
 def getDigit(string, pos):
     return int(string[pos % len(string)])
 
-input = fileinput.input()[0][:-1:] #input from AOC will always be on a single line, including a \n
+#  input from AOC day 1 will always be on a single line, including a \n
+puzInput = fileinput.input().readline()[:-1:]
 
 runningSum = 0
 
-for i in range(0, len(input)):
-    if(getDigit(input,i) == getDigit(input,i+len(input)//2)):
-        runningSum += getDigit(input,i)
-        
-print(runningSum)
+for i in range(0, len(puzInput)):
+    if(getDigit(puzInput, i) == getDigit(puzInput, i + len(puzInput) // 2)):
+        runningSum += getDigit(puzInput, i)
 
+print(runningSum)
