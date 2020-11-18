d95b4cd93c53444369b2e35afb5b1eaa813df3c6
DecafMegalodon <5885124+DecafMegalodon@users.noreply.github.com>
2020-11-17 19:34:32 -0500

    Test automation implemented, just need to add tests

--- test_all.py
+++ test_all.py
@@ -3,18 +3,26 @@ import fileinput
 import subprocess
 import time
 
+# output = subprocess.run(args=["python3", 'day01b.py'], stdin=ioPipe, capture_output=True)
+# print(output.stdout)
+results = open("inputs/results.txt")
 
-ioPipe = open("inputs/day01.txt")
-
-start = time.time()
-print(start)
-output = subprocess.run(args=["python3", 'day01b.py'], stdin=ioPipe, capture_output=True)
-print(output.stdout)
-
-# for day in range(1,25+1):
-    # for part in ('a', 'b'):
-        # if day == 25 and part == 'b': #  No part b on on 25th
-            # print("Tests complete!")
-            # exit(0)
-        # print(str(day) + part + '.py')
-        
+for day in range(1,3+1):
+    paddedDay = ('0' if day < 10 
+                else '') + str(day)
+    dayInput = open("inputs/day" + paddedDay + ".txt")
+    for part in ('a', 'b'):
+        if day == 25 and part == 'b': #  No part b on on 25th
+            print("Tests complete!")
+            exit(0)
+        progName = ''.join(['day', paddedDay, part, '.py'])
+        startTime = time.time()
+        subProc = subprocess.run(args=["python3", progName],
+                                stdin=dayInput, capture_output=True)
+        stopTime = time.time()
+        output = str(subProc.stdout.decode('utf-8')).strip('\n')
+        expected = results.readline().strip('\n')
+        result = "OK" if output == expected else "FAIL"
+        timeTaken = round(stopTime - startTime, 4)
+        print("D" + paddedDay + part, str(result), timeTaken, "seconds")
+        dayInput.seek(0,0)
